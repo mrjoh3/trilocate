bce0f07814c040405ca1163ad42eaadf3ef8db7a
Matt Johnson <matthew.johnson@epa.vic.gov.au>
2019-12-20 19:17:15 +1100

    basic app and auto form generate

--- app.R
+++ app.R
@@ -8,43 +8,114 @@
 #
 
 library(shiny)
+library(shinydashboard)
+library(shinythemes)
+library(shinyWidgets)
+library(shinyalert)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+library(sf)
+library(leaflet)
+library(glue)
+library(dplyr)
+
+towers <- readRDS('towers.rds')
+
+APP_TITLE <- 'Smoke Locate'
+
+
+ui <- shinyUI(fluidPage(
+      title = APP_TITLE,
+      #responsive = TRUE,
+      theme = shinytheme("superhero"),
+      header = NULL,
+      useShinydashboard(),
+      tags$div(style = 'text-align: center;',
+               h1(APP_TITLE, icon('fire', class = 'orange')),
+               #h5(textOutput('subtitle')),
+               tags$hr()
       ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+      includeCSS('www/css/styles.css'),
+      fluidRow(
+         leafletOutput('map'),
+         tags$hr()
+      ),
+      fluidRow(
+         uiOutput('towers')
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+   selected <- reactiveValues(towers = c())
+   
+   output$map <- renderLeaflet({
+
+      leaflet() %>%
+         addTiles() %>%
+         addMarkers(data = towers, layerId = ~ FEATURE_ID)
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
+   
+   observe({
+      
+      proxy <- leafletProxy("map")
+      
+   })
+   
+   observeEvent(input$map_marker_click, {
+
+      click <- input$map_marker_click
+      
+      isolate(selected$towers <- c(selected$towers, click$id))
+      
+   })
+   
+   observe({
+      
+      # output$towers <- renderUI({
+      #    h3(glue('seems to work {paste(selected$towers, collapse = ", ")}'))
+      # })
+      
+      
+      # construct UI
+      output$towers <- renderUI({
+         
+         if (length(selected$towers) == 0) {
+            
+            column(12, h3('Please Select 1 or more Towers'))
+            
+         } else {
+            
+            tagList(lapply(1:length(selected$towers), function(n){
+               
+               id <- selected$towers[n]
+               r <- filter(towers, FEATURE_ID == id)
+               name <- r[['NAME_LABEL']]
+               
+               column(12 / length(selected$towers),
+                      h3(name),
+                      numericInput(glue('bearing_{id}'), label = 'Bearing', value = NULL),
+                      sliderInput(glue('vis_{id}'), label = 'Visibility (km)', min = 0, max = 50, step = 5, value = 20), 
+                      tags$hr()
+                      )
+               
+            }))
+            
+         }
+
+         
+         
+      })
+      
+      
+      # render forms
+      
+   })
+   
 }
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
 
--- process_spatial.R
+++ process_spatial.R
@@ -16,18 +16,11 @@ cfa_tfb <- st_read('shp/cfa_tfb_district.shp', stringsAsFactors = FALSE)
 
 twr <- st_read('shp/geomark_point.shp', stringsAsFactors = FALSE) %>%
   filter(STATE == 'VIC',
-         FEATSUBTYP == 'fire lookout',
-         !grepl('SATELLITE|&|infrastructure', NAME, ignore.case = TRUE)) %>%
-  select(town_name = NAME_LABEL) %>%
-  mutate(town_name = str_trim(gsub('Fire Station|District', '', town_name), 'both'),
-         town_name = gsub('\\.', '', town_name),
-         town_val = tolower(gsub(' | - ', '-', town_name))) %>%
-  st_join(cfa_tfb) %>%
-  select(town_name,
-         town_val,
-         cfa_tfb = TFB_DIST) %>%
-  mutate(cfa_tfb = tools::toTitleCase(tolower(cfa_tfb))) %>%
+         FEATSUBTYP == 'fire lookout') %>%
   st_cast('POINT')
+
+
+saveRDS(twr, 'towers.rds')
 
 
 
@@ -44,5 +37,5 @@ l1 <- st_linestring(rbind(kg, destPoint(kg, 40, vis)))
 
 l2 <- st_linestring(rbind(le, destPoint(le, 40, vis))) 
 
-saveRDS(twr, 'towers.rds')
+
 
